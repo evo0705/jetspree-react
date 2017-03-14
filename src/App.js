ad2491c76f9646eb6c42a6947b60e649bd2e09f7
evo0705 <evo0705@gmail.com>
2017-03-14 15:07:45 +0800

    added landing and browse country

--- src/App.js
+++ src/App.js
@@ -1,86 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Select from 'react-select';
 import '../node_modules/react-select/dist/react-select.min.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import { loadCountries, loadSubCategories } from './data/common.js';
-import { loadRequests } from './data/requests.js';
-
+import Landing from './routes/Landing.js';
 import BrowseCountries from './routes/requests/Countries.js';
 
-class App extends Component {
-
-	constructor (props) {
-		super(props)
-		this.state = {
-		  requests: '',
-		  name: 'cake',
-		  category: 'snacks',
-		  country: 'my',
-		  countries: [],
-		  categories: []
-		};
-		this.buttonClick = this.buttonClick.bind(this);
-		this.inputChange = this.inputChange.bind(this);
-		this.changeCategory = this.changeCategory.bind(this);
-		this.changeCountry = this.changeCountry.bind(this);
-	}
-	
-	loadData(pagesize){
-		var scope = this;		
-		let param = {
-			name: scope.state.name,
-			category: scope.state.category,
-			pagesize: pagesize
-		};
-		loadRequests(param).then((data) => {
-			scope.setState({requests: JSON.stringify(data)});
-		});
-	}
-	
-	initData(){
-		var scope = this;
-		
-		loadCountries().then((data) => { 
-			scope.setState({
-				countries: data.map(function(obj){ 
-					return { label: obj.name, value: obj._id } 
-				})
-			});
-		});
-		loadSubCategories().then((data) => { 
-			scope.setState({
-				categories: data.map(function(obj){
-					return { label: obj.name, value: obj._id } 
-				})
-			});
-		});
-	}
-
-  componentDidMount() {
-    this.initData();
-  }
-  
-  buttonClick(pagesize){
-	this.setState({requests: ""});
-	this.loadData(pagesize);
-  }
-  
-  inputChange(event) {
-    this.setState({name: event.target.value});
-  }
-  
-  changeCategory(val) {
-	this.setState({category: val.value});
-  }
-  
-  changeCountry(val) {
-	this.setState({country: val.value});
-  }
-  
-  
+class App extends Component {  
   
   render() {
     return (
@@ -90,19 +17,13 @@ class App extends Component {
 			  <img src={logo} className="App-logo" alt="logo" />
 			  <h2>Welcome to React123</h2>
 			</div>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/countries">Browse by Countries</Link></li>
+			</ul>
 			
-			<Link to="/countries">Browse by Countries</Link>
+			<Route exact path="/" component={Landing}/>
 			<Route path="/countries" component={BrowseCountries}/>
-			
-			<p className="App-intro">
-			  To get started, edit <code>src/App.js</code> and save to reload.
-			</p>
-			<label>Name:</label><input type="text" value={this.state.name} onChange={this.inputChange} /><br />
-			<label>Country</label><Select name="form-country" searchable={false} clearable={false} value={this.state.country} options={this.state.countries} onChange={this.changeCountry} />
-			<label>Category</label><Select name="form-category" searchable={false} clearable={false} value={this.state.category} options={this.state.categories} onChange={this.changeCategory} />
-			<input type="button" onClick={() => this.buttonClick(100)} value="Get 100 records" />
-			<input type="button" onClick={() => this.buttonClick(1000)} value="Get 1000 records!" />
-			<pre>{this.state.requests}</pre>
 		  </div>
 		</Router>
     );
---
+++ src/routes/Landing.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import Select from 'react-select';
+import { loadCountries, loadSubCategories } from '../data/common.js';
+import { loadRequests } from '../data/requests.js';
+
+class Landing extends React.Component {
+
+	constructor (props) {
+		super(props)
+		this.state = {
+		  requests: '',
+		  name: 'cake',
+		  category: 'snacks',
+		  country: 'my',
+		  countries: [],
+		  categories: []
+		};
+		this.buttonClick = this.buttonClick.bind(this);
+		this.inputChange = this.inputChange.bind(this);
+		this.changeCategory = this.changeCategory.bind(this);
+		this.changeCountry = this.changeCountry.bind(this);
+	}
+	
+	loadData(pagesize){
+		var scope = this;		
+		let param = {
+			name: scope.state.name,
+			category: scope.state.category,
+			pagesize: pagesize
+		};
+		loadRequests(param).then((data) => {
+			scope.setState({requests: JSON.stringify(data)});
+		});
+	}
+	
+	initData(){
+		var scope = this;
+		
+		loadCountries().then((data) => { 
+			scope.setState({
+				countries: data.map(function(obj){ 
+					return { label: obj.name, value: obj._id } 
+				})
+			});
+		});
+		loadSubCategories().then((data) => { 
+			scope.setState({
+				categories: data.map(function(obj){
+					return { label: obj.name, value: obj._id } 
+				})
+			});
+		});
+	}
+
+	componentDidMount() {
+		this.initData();
+	}
+	  
+	buttonClick(pagesize){
+		this.setState({requests: ""});
+		this.loadData(pagesize);
+	}
+	 
+	inputChange(event) {
+		this.setState({name: event.target.value});
+	}
+	  
+	changeCategory(val) {
+		this.setState({category: val.value});
+	}
+	  
+	changeCountry(val) {
+		this.setState({country: val.value});
+	}
+	
+	render(){
+		return(
+			<div>			
+				<label>Name:</label><input type="text" value={this.state.name} onChange={this.inputChange} /><br />
+				<label>Country</label><Select name="form-country" searchable={false} clearable={false} value={this.state.country} options={this.state.countries} onChange={this.changeCountry} />
+				<label>Category</label><Select name="form-category" searchable={false} clearable={false} value={this.state.category} options={this.state.categories} onChange={this.changeCategory} />
+				<input type="button" onClick={() => this.buttonClick(100)} value="Get 100 records" />
+				<input type="button" onClick={() => this.buttonClick(1000)} value="Get 1000 records!" />
+				<pre>{this.state.requests}</pre>
+			</div>
+		);
+	}
+}
+export default Landing;
