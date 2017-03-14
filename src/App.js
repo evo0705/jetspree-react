09f377fcb0976720b98ed709150cf7168b5a4a88
blackyu321 <yuho.chiu@jetspree.com>
2017-03-14 15:19:04 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Select from 'react-select';
 import '../node_modules/react-select/dist/react-select.min.css';
 import { loadCountries, loadSubCategories } from './data/common.js';
 import { loadRequests } from './data/requests.js';
+import Home from './home/Home.js';
+
 
 class App extends Component {
 
@@ -76,6 +78,10 @@ class App extends Component {
 	this.setState({country: val.value});
   }
   
+renderHome() {
+	return <Home />
+}
+
   render() {
     return (
       <div className="App">
@@ -83,6 +89,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React123</h2>
         </div>
+
+{renderHome()}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+import { loadCountries, loadSubCategories } from '../data/common.js';
+import { loadRequests } from '../data/requests.js';
+
+
+class Home extends Component {
+	render() {
+		return (
+<div>
+jhello
+</div>
+			)
+	}
+
+}
