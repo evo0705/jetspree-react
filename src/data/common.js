d5b178f101ec663c0a6992c75c82caca1e7e696e
evo0705 <evo0705@gmail.com>
2017-04-10 17:29:26 +0800

    use config vars for api path

--- src/data/common.js
+++ src/data/common.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export function loadCountries() {
-	return Axios.get('https://jetspree-node-test.herokuapp.com/countries')
+    return Axios.get(process.env.REACT_APP_JETSPREE_API_URL + '/countries')
 		.then( (response) => response.data )
 		.catch(function (error) {
 			console.log(error);
@@ -9,7 +9,7 @@ export function loadCountries() {
 }
 
 export function loadSubCategories() {
-	return Axios.get('https://jetspree-node-test.herokuapp.com/categories/sub')
+    return Axios.get(process.env.REACT_APP_JETSPREE_API_URL + '/categories/sub')
 		.then( (response) => response.data )
 		.catch(function (error) {
 			console.log(error);
