f05e9ff9382f8e422313e6b91048ae5914305bc4
DIJAYACORP\j.donald <xetro90@gmail.com>
2017-04-18 17:14:37 +0800

    hardcoded data

---
+++ src/data/hardcoded-data.js
@@ -0,0 +1,36 @@
+
+// All hardcoded data
+
+export function loadCountries() {
+	return [
+		{
+			code: 'my',
+			name: 'malaysia'
+		},
+		{
+			code: 'hk',
+			name: 'hong kong'
+		},
+		{
+			code: 'sg',
+			name: 'singapore'
+		}
+	]
+}
+
+export function loadCurrencies() {
+	return [
+		{
+			code: 'myr',
+			name: 'Malaysian Ringgit'
+		},
+		{
+			code: 'usd',
+			name: 'US Dollar'
+		},
+		{
+			code: 'hkd',
+			name: 'Hong Kong Dollar'
+		}
+	]
+}
